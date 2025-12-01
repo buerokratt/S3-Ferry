889569d425c4a35af811650b871329cf83db4500
Igor Krupenja <igor.krupenja@gmail.com>
2025-12-01 17:53:56 +0200

    feat(798): readme

--- src/services/app.service.ts
+++ src/services/app.service.ts
@@ -97,7 +97,6 @@ export class AppService {
     }
   }
 
-  // todo custom url? investigate how used with s3
   async createFile(data: CreateFileBodyDto): Promise<void> {
     try {
       // Create files at all specified locations with the same content in parallel
