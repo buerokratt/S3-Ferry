999d7b61291046ce1649db79115299b9894a279a
Igor Krupenja <igor.krupenja@gmail.com>
2025-12-03 12:04:43 +0200

    feat(798): required

--- src/s3/config/s3.config.schema.ts
+++ src/s3/config/s3.config.schema.ts
@@ -1,11 +1,11 @@
 import * as joi from 'joi';
 
 const schema = {
-  S3_REGION: joi.string().required(),
-  S3_ENDPOINT_URL: joi.string().uri().required().allow(''),
-  S3_ACCESS_KEY_ID: joi.string().required(),
-  S3_SECRET_ACCESS_KEY: joi.string().required(),
-  S3_DATA_BUCKET_NAME: joi.string().required(),
+  S3_REGION: joi.string(),
+  S3_ENDPOINT_URL: joi.string().uri().allow(''),
+  S3_ACCESS_KEY_ID: joi.string(),
+  S3_SECRET_ACCESS_KEY: joi.string(),
+  S3_DATA_BUCKET_NAME: joi.string(),
   S3_DATA_BUCKET_PATH: joi.string().allow(''),
 };
 
